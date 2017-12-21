500b6a210abb31402a745ba61edbb7d7cf5eea81
Bikram Nayak <Bikram.Nayak@gep.com>
2017-12-21 17:34:51 +0530

    localstorage

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,19 @@
 var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world !';
+var myButton = document.querySelector('button');
+
+
+function setUsername(){
+    var myName = prompt('Please enter ur name.');
+    localStorage.setItem('userName',myName);
+    myHeading.textContent = "Mozilla is cool! "+myName;    
+}
+
+if(!localStorage.getItem('userName')){
+    setUsername()
+}else{
+    myHeading.textContent = "Mozilla is cool! "+localStorage.getItem('userName');
+}
+
+myButton.onclick = function(){
+    setUsername();
+}
